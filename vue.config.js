194d601861fdbba2cee3d2c018cd9d8f8849d86b
LianLambert <lian1lambert@gmail.com>
2025-01-03 15:55:20 -0500

    feat: add under construction message

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
-const { defineConfig } = require('@vue/cli-service');
+const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/lian-website/'
-    : '/',
+  publicPath: process.env.NODE_ENV === "production"
+    ? "/lian-website/"
+    : "/",
 
   transpileDependencies: true
 });

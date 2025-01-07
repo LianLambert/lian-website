20e069b88606b02a53d5686892d0612ce66ccfbb
LianLambert <lian1lambert@gmail.com>
2025-01-06 21:28:05 -0500

    feat: create global.css

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
   <div id="app">
     <MainNavBar />
-    <div id="main-container">
-      <section id="about-section" style="height: 300px">About Section</section>
-      <section id="experience-section" style="height: 300px">
-        Experience Section
-      </section>
-      <section id="projects-section" style="height: 300px">Projects Section</section>
-      <section id="abilities-section" style="height: 300px">Abilities Section</section>
-      <section id="contact-section" style="height: 300px">Contact Section</section>
+    <div id="sections-container">
+      <section id="about" class="section">About Section</section>
+      <section id="experience" class="section">Experience Section</section>
+      <section id="projects" class="section" style="background-color: black">Projects Section</section>
+      <section id="abilities" class="section">Abilities Section</section>
+      <section id="contact" class="section">Contact Section</section>
     </div>
   </div>
 </template>
@@ -23,13 +21,8 @@ export default {
   },
 };
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.section {
+  height: 400px;
 }
 </style>

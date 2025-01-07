26556d5df6619e6c97d0da95c6bbd9ad20d7edd1
LianLambert <lian1lambert@gmail.com>
2025-01-07 15:21:58 -0500

    feat: create AbilityRating.vue

---
+++ src/components/AbilityRating.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="d-flex justify-content-between align-items-center w-100">
+    <span>{{ ability }}</span>
+    <StarRating :rating="rating" />
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+import StarRating from "./StarRating.vue";
+
+defineProps({
+  ability: {
+    required: true,
+    type: String,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    validator: (value) => value >= 0 && value <= 5,
+  },
+});
+</script>
+
+<style scoped></style>

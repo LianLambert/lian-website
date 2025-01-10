c6ae83d8ec7b231e23eb16f06c1177d1de7482d6
LianLambert <lian1lambert@gmail.com>
2025-01-10 11:09:31 -0500

    feat: draft ProjectSection.vue (forgot to add)

--- src/components/sections/ProjectSection.vue
+++ src/components/sections/ProjectSection.vue
@@ -1,5 +1,29 @@
 <template>
-  <section id="project" class="section" style="background-color: #ffffff">Project Section!</section>
+  <section id="experience" class="section centered" style="background-color: #ffffff">
+    <div class="project-container row">
+      <div><h1>Projects</h1></div>
+      <ProjectEntry
+        title="The Adventures of Super Cool Carrot Rocket Chessmaster Girl"
+        link="https://lianl.itch.io/the-adventures-of-super-cool-carrot-rocket-chessmaster-girl"
+        :image="lianImage"
+        description="I did some stuff, hopefully you guys like it! What else do I write here? Seriously lol"
+      />
+    </div>
+  </section>
 </template>
-<script setup></script>
-<style scoped></style>
+<script setup>
+import ProjectEntry from "../ProjectEntry.vue";
+
+// to do: use correct images
+import lianImage from "../../assets/lian.jpg";
+</script>
+<style scoped>
+.project-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  width: 100%;
+  max-width: 1100px;
+}
+</style>

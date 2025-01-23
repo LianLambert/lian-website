c04e7f8bb6e086fa902cedec810060f300da0404
Lian L <lian1lambert@gmail.com>
2025-01-23 18:09:39 -0500

    feat: project section -!MISSING DEMO LINKS

--- src/components/ProjectEntry.vue
+++ src/components/ProjectEntry.vue
@@ -1,26 +1,39 @@
 <template>
-  <div class="project-entry centered col-12 col-lg-6">
-    <div class="project-entry-image-container centered">
-      <img :src="image" class="project-entry-image" />
-    </div>
-    <div class="project-entry-details">
-      <h3>
-        <a :href="link" target="_blank">{{ title }}</a>
-      </h3>
-      <div>{{ description }}</div>
-    </div>
+  <div class="col-12 col-lg-6 p-0">
+    <a :href="linkToOpen" target="_blank" class="project-entry centered">
+      <div class="project-entry-image-container">
+        <img :src="image" class="project-entry-image" />
+      </div>
+      <div class="project-entry-details">
+        <h3 class="mb-2 grow-on-hover">{{ title }}</h3>
+        <div class="grow-on-hover">{{ description }}</div>
+        <div 
+          v-if="props.demoLink" 
+          class="watch-demo" 
+          @mouseenter="onWatchDemoEnter" 
+          @mouseleave="onWatchDemoExit"
+          @touchstart="onWatchDemoEnter" 
+          @touchend="onWatchDemoExit">
+          Watch Demo
+        </div>
+      </div>
+    </a>
   </div>
 </template>
 <script setup>
 import { defineProps } from "vue";
 
-defineProps({
+const props = defineProps({
   title: {
     required: true,
     type: String,
   },
   link: {
     required: true,
+    type: String,
+  },
+  demoLink: {
+    required: false,
     type: String,
   },
   image: {
@@ -32,19 +45,62 @@ defineProps({
     type: String,
   },
 });
+
+import { ref } from "vue";
+
+const linkToOpen = ref(props.link);
+
+// to do: fix this for mobile too
+function onWatchDemoEnter() {
+  linkToOpen.value = props.demoLink;
+}
+
+function onWatchDemoExit() {
+  linkToOpen.value = props.link;
+}
+
 </script>
 <style scoped lang="scss">
 .project-entry {
-  margin: 15px 0;
+  margin: 15px;
+  padding: 10px;
+  border-radius: 10px;
+  border: 4px solid black;
+  color: var(--dark-purple);
+  box-shadow: 0 8px 10px rgba(0, 0, 0, 0.3);
+  
+
+  &:hover {
+    background-color: black;
+    transform: scale(1.01) translateY(-10px);
+    color: var(--medium-purple);
+    box-shadow: 0 12px 20px rgba(0, 0, 0, 0.3);
+
+    .watch-demo {
+      margin-top: 10px;
+      border-radius: 5px;
+      padding: 5px 15px;
+      color: var(--dark-purple);
+      background-color: var(--medium-purple);
+    }
+  }
+
+  @media (max-width: 767px) {
+    flex-direction: column;
+  }
 }
 
 .project-entry-image-container {
   padding: 10px;
+
+  &:hover {
+    transform: scale(1.05);
+  }
 }
 
 .project-entry-image {
   width: 200px;
-  height: 200px;
+  height: auto;
   object-fit: cover;
   border-radius: 10%;
   overflow: hidden;
@@ -57,5 +113,20 @@ defineProps({
   justify-content: start;
   align-items: start;
   text-align: left;
+}
+
+.grow-on-hover:hover {
+  transform: scale(1.05);
+}
+
+.watch-demo {
+  font-size: 17px;
+  font-weight: 700;
+  color: black;
+  padding: 5px 0;
+
+  &:hover {
+    transform: scale(1.05);
+  }
 }
 </style>
--- src/components/sections/ProjectSection.vue
+++ src/components/sections/ProjectSection.vue
@@ -1,30 +1,31 @@
 <template>
-  <section id="project" class="section centered" style="background-color: #ffffff">
+  <section id="project" class="section centered">
     <div id="project-container" class="row">
-      <div><h1 class="h1-light-bg">Projects</h1></div>
+      <div><h1 class="h1-light-bg" style="color: black">Projects</h1></div>
       <ProjectEntry
         title="Lost At Sea"
         link="https://lianl.itch.io/lost-at-sea"
+        demo-link="https://www.sunnypatel.net/#projects"
         :image="LostAtSea"
-        description="A minesweeper inspired game with a nautical twist! Use lighthouses to find the lost boats but beware of sharks! Created for McGameJam 2025 and made with Unity (C#) in < 48h."
+        description="A minesweeper inspired game with a nautical twist!Created for McGameJam 2025 and made with Unity (C#) in < 48h."
       />
       <ProjectEntry
         title="Commit2Gather"
         link="https://github.com/lil1an/commit2gather/tree/main"
         :image="Commit2Gather"
-        description="A booking software to help McGill University students and staff schedule and manage their appointments. Created for COMP 307 - Principles of Web Development."
+        description="An appointment booking software to help McGill University students and staff. Created for COMP 307 - Principles of Web Development."
       />
       <ProjectEntry
         title="The Adventures of Super Cool Carrot Rocket Chessmaster Girl"
-        link="https://docs.google.com/presentation/d/1hII6qBH_dLbKPqPi4oqcR_595N9Twu2WiQ7Q0VdtBjU/edit"
+        link="https://lianl.itch.io/the-adventures-of-super-cool-carrot-rocket-chessmaster-girl"
         :image="TASCCRCG"
         description="A 2D pixel art shoot’em up game! Created for McGameJam 2024 and made with Unity (C#) in < 48h."
       />
       <ProjectEntry
         title="Game Design Portfolio"
-        link="https://lianl.itch.io/the-adventures-of-super-cool-carrot-rocket-chessmaster-girl"
+        link="https://docs.google.com/presentation/d/1hII6qBH_dLbKPqPi4oqcR_595N9Twu2WiQ7Q0VdtBjU/edit"
         :image="Portfolio"
-        description="Applied game design principles to enhance Don't Fall Down! in collaboration with the developer, with updates featured in Version 1.2 and designed a level inspired by Fall Guys."
+        description="A portfolio highlighting my contributions to the ‘Don't Fall Down!' App with game design updates in Version 1.2 and a Fall Guys-inspired level design."
       />
     </div>
   </section>
@@ -32,16 +33,15 @@
 <script setup>
 import ProjectEntry from "../ProjectEntry.vue";
 
-// to do: use correct images
-import LostAtSea from "../../assets/lost-at-sea_v.jpg";
-import Commit2Gather from "../../assets/Commit2Gather_v.jpg";
-import TASCCRCG from "../../assets/TASCCRCG_v.jpg";
-import Portfolio from "../../assets/portfolio_v.jpg";
-
+import LostAtSea from "../../assets/lost-at-sea.png";
+import Commit2Gather from "../../assets/Commit2GatherRed.png";
+import TASCCRCG from "../../assets/TASCCRCG.png";
+import Portfolio from "../../assets/portfolio.png";
 </script>
 <style scoped lang="scss">
 #project {
   padding: 4vw 2vw;
+  background-color: #f2f2f2;
 }
 
 #project-container {

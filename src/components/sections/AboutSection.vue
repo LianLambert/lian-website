96b0ca9d2d744703e6c8ac0db0ff64fa56dafe12
Lian L <lian1lambert@gmail.com>
2025-01-22 22:09:02 -0500

    feat: really good about page

--- src/components/sections/AboutSection.vue
+++ src/components/sections/AboutSection.vue
@@ -1,11 +1,11 @@
 <template>
   <section id="about" class="section">
-    <img id="lian-pose" :src="LianPose" />
-    <div id="text-container">
+    <img id="lian-pose" :src="LianPose" alt="Lian striking a pose"/>
+    <div id="text-container" :class="{ 'showAnimationPulse': showAnimationPulse }">
       <span id="hey-there">Hey there! &nbsp;I'm Lian</span>
       <div class="row">
         <div class="col-12 col-md-4 details-group">
-          <p>I'm a recent graduate from<br><b>McGill</b> with a double major in <b>Software Engineering</b> and <b>Psychology</b>. I love blending tech with human behavior to create designs that make an impact! I’m passionate about <b>web dev</b>, &nbsp;<b>game dev</b> and anything that lets me use my creativity!</p>
+          <p>I'm a recent graduate from<br class="conditional-br"><b>McGill</b> with a double major in <b>Software Engineering</b> and <b>Psychology</b>. I love blending tech with human behavior to create designs that make an impact! I’m passionate about <b>web dev</b>, &nbsp;<b>game dev</b> and anything that lets me use my creativity!</p>
         </div>
         <div class="col-12 col-md-4"></div>
         <div class="col-12 col-md-4" style="padding: 0">
@@ -23,13 +23,13 @@
           </div>
           <div class="d-flex align-items-center details-group">
             <span class="details-label">Links: &nbsp;</span>
-            <a class="icon-container" href="/Lian_Lambert_Resume_2025.pdf" target="_blank">
+            <a class="icon-container" href="/Lian_Lambert_Resume_2025.pdf" target="_blank" alt="Resume icon">
               <img src="../../assets/resume.svg" class="icon"/>
             </a>
-            <a class="icon-container" href="https://github.com/LianLambert" target="_blank">
+            <a class="icon-container" href="https://github.com/LianLambert" target="_blank" alt="Github icon">
               <img src="../../assets/github.svg" class="icon"/>
             </a>
-            <a class="icon-container" href="https://www.linkedin.com/in/lian-lambert/" target="_blank">
+            <a class="icon-container" href="https://www.linkedin.com/in/lian-lambert/" target="_blank" alt="LinkedIn icon">
               <img src="../../assets/linkedin.svg" class="icon"/>
             </a>
          </div>
@@ -44,6 +44,7 @@ import LianPose from "../../assets/lian-pose.png"
 
 const navbar = ref(null);
 const aboutSection = ref(null);
+const showAnimationPulse = ref(false);
 
 const birthYear = 2001;
 const currentYear = new Date().getFullYear();
@@ -58,6 +59,14 @@ onMounted(() => {
     const viewportHeight = window.innerHeight;
     aboutSection.value.style.height = `${viewportHeight - 35}px`;
   }
+
+  setTimeout(() => {
+    showAnimationPulse.value = true;
+
+    setTimeout(() => {
+      showAnimationPulse.value = false;
+    }, 600);
+  }, 1000);
 });
 </script>
 <style scoped lang="scss">
@@ -91,6 +100,11 @@ onMounted(() => {
     box-shadow: 0 8px 20px rgba(0, 0, 0, 0.3);
   }
 
+  @media(max-width: 992px) {
+    &:hover ~ #text-container {
+      transform: scale(1.05) rotate(-1.5deg);
+    }
+  }
 
   @media (max-width: 767px) {
     height: auto;
@@ -112,6 +126,15 @@ onMounted(() => {
     transform: scale(1.1) rotate(-1.5deg);
     box-shadow: 0 8px 20px rgba(0, 0, 0, 0.3);
   }
+
+  @media (max-width: 992px) {
+    padding: 3vw 2vw;
+
+    &:hover {
+      transform: scale(1.05) rotate(-1.5deg);
+      box-shadow: 0 8px 20px rgba(0, 0, 0, 0.3);
+    }
+  }
 }
 
 #hey-there {
@@ -122,6 +145,13 @@ onMounted(() => {
   &:hover {
     transform: scale(1.1);
     color: white;
+  }
+
+  @media (max-width: 992px) {
+    &:hover {
+      transform: scale(1.05);
+      color: white;
+    }
   }
 }
 
@@ -163,10 +193,33 @@ onMounted(() => {
       filter: brightness(0) saturate(100%) invert(14%) sepia(20%) saturate(3529%) hue-rotate(237deg) brightness(90%) contrast(100%);
     }
   }
+
+  @media (max-width: 992px) {
+    font-size: 15px;
+
+    b {
+      margin: 3px;
+      font-size: 17px;
+    }
+
+    &:hover {
+        transform: scale(1.05);
+
+        b {
+          padding: 3px 5px;
+        }
+        
+        .icon-container {
+          padding: 0px;
+          margin: 0 3px;
+        }
+      }
+  }
 }
 
 .icon-container:hover {
-    transform: scale(1.1);
+  transform: scale(1.1);
+
 }
 
 .icon {
@@ -174,12 +227,48 @@ onMounted(() => {
   filter: brightness(0) saturate(100%) invert(91%) sepia(88%) saturate(7026%) hue-rotate(216deg) brightness(80%) contrast(84%);
   width: 25px;
   height: 25px;
-  margin: 5px 10px 5px 10px;
+  margin: 5px 10px;
+
+  @media (max-width: 992px) {
+    width: 21px;
+    height: 21px;
+    margin: 3px 6px;
+  }
 }
 
 .details-label {
   font-size: 23px;
   font-weight: bold;
   color:var(--light-purple);
+
+  @media (max-width: 992px) {
+    font-size: 20px;
+  }
+}
+
+.conditional-br {
+  display: inline;
+}
+
+@media (max-width: 992px) {
+  .conditional-br {
+    display: none;
+  }
+}
+
+.showAnimationPulse {
+  animation: scaleAndTilt 0.6s ease-out;
+}
+
+@keyframes scaleAndTilt {
+  0% {
+    transform: scale(1) rotate(0deg);
+  }
+  50% {
+    transform: scale(1.1) rotate(-1.5deg);
+  }
+  100% {
+    transform: scale(1) rotate(0deg);
+  }
 }
 </style>

8c8b2f8aa626094e3b30d049b9a88e4c7b5b0b63
Lian L <lian1lambert@gmail.com>
2025-01-24 14:21:18 -0500

    feat: update project links

--- src/components/sections/ProjectSection.vue
+++ src/components/sections/ProjectSection.vue
@@ -4,28 +4,38 @@
       <div><h1 class="h1-light-bg" style="color: black">Projects</h1></div>
       <ProjectEntry
         title="Lost At Sea"
-        link="https://lianl.itch.io/lost-at-sea"
-        demo-link="https://www.sunnypatel.net/#projects"
         :image="LostAtSea"
         description="A minesweeper inspired game with a nautical twist!Created for McGameJam 2025 and made with Unity (C#) in < 48h."
+        primaryLink="https://lianl.itch.io/lost-at-sea"
+        demoLink="https://www.youtube.com/watch?v=FVWS2cOupnk&ab_channel=Lian"
+        githubLink="https://github.com/LianLambert/Lost-At-Sea-McGameJam2025"
+        otherLink="https://lianl.itch.io/lost-at-sea"
       />
       <ProjectEntry
         title="Commit2Gather"
-        link="https://github.com/lil1an/commit2gather/tree/main"
         :image="Commit2Gather"
         description="An appointment booking software to help McGill University students and staff. Created for COMP 307 - Principles of Web Development."
+        primaryLink="https://github.com/lil1an/commit2gather"
+        demoLink="https://www.youtube.com/watch?v=-zRBUqTmG-U&t=3s&ab_channel=Lian"
+        githubLink="https://github.com/lil1an/commit2gather"
       />
       <ProjectEntry
         title="The Adventures of Super Cool Carrot Rocket Chessmaster Girl"
-        link="https://lianl.itch.io/the-adventures-of-super-cool-carrot-rocket-chessmaster-girl"
         :image="TASCCRCG"
         description="A 2D pixel art shoot’em up game! Created for McGameJam 2024 and made with Unity (C#) in < 48h."
+        primaryLink="https://lianl.itch.io/the-adventures-of-super-cool-carrot-rocket-chessmaster-girl"
+        demoLink="https://www.youtube.com/watch?v=ISyCelCeTNc&t=9s&ab_channel=Lian"
+        githubLink="https://github.com/LianLambert/The-Adventures-of-Super-Cool-Carrot-Rocket-Chessmaster-Girl-McGameJam2024"
+        otherLink="https://lianl.itch.io/the-adventures-of-super-cool-carrot-rocket-chessmaster-girl"
+        
       />
       <ProjectEntry
         title="Game Design Portfolio"
-        link="https://docs.google.com/presentation/d/1hII6qBH_dLbKPqPi4oqcR_595N9Twu2WiQ7Q0VdtBjU/edit"
         :image="Portfolio"
         description="A portfolio highlighting my contributions to the ‘Don't Fall Down!' App with game design updates in Version 1.2 and a Fall Guys-inspired level design."
+        primaryLink="https://docs.google.com/presentation/d/1hII6qBH_dLbKPqPi4oqcR_595N9Twu2WiQ7Q0VdtBjU/edit#slide=id.gace710277d_0_491"
+        otherLink="https://docs.google.com/presentation/d/1hII6qBH_dLbKPqPi4oqcR_595N9Twu2WiQ7Q0VdtBjU/edit#slide=id.gace710277d_0_491"
+        
       />
     </div>
   </section>

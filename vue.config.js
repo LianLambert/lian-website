21ad891ade82845aa4aef4dd024f885dc5597b8c
Lian L <lian1lambert@gmail.com>
2025-02-04 15:35:11 -0500

    feat: fix repo name in config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/lian-website/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/website/" : "/",
   transpileDependencies: true,
 });
